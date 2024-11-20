c59961892bb3828255fd6d465d0a494e5f857dc9
MatthewPCope <matthew.p.cope@gmail.com>
2024-11-19 16:29:03 -0800

    meals css

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import classes from './page.module.css'
+import Image from 'next/image'
 
 const mealsDetailsPage = () => {
   return (
-    <div>Meal Details</div>
+    <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image fill/>
+      </div>
+      <div className={classes.headerText}>
+        <h1>TITLE</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+        </p>
+        <p className={classes.summary}>SUMMARY</p>
+      </div>
+    </header>
+    <main>
+      <p className={classes.instructions}
+      dangerouslySetInnerHTML={{
+        __html: '...'
+      }}></p>
+    </main>
+  </>
   )
 }
 
